141e3a888079541f366715a20e932f34f66ebe89
John Stevenson <john@jr0cket.co.uk>
2019-01-01 12:27:46 +0000

    Documented project and sections of code
    Described the purpose of the project.
    
    Created sections in the code to make it easier for humans to follow.

--- src/clojure_virtual_study_group/core.cljs
+++ src/clojure_virtual_study_group/core.cljs
@@ -1,19 +1,53 @@
+;; Clojure Virtual Study Group website
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; A website to guide developers into Clojure via video broadcasting
+;; Videos are published on YouTube in the jr0cket channel and as a playlist
+;; The study group website will allow people to search through the content
+;; of each video and find the parts they want to learn next.
+;; This website will contain full details to help people follow them study group,
+;; including code examples, descriptions, transcripts, etc.
+
+
 (ns clojure-virtual-study-group.core
     (:require [reagent.core :as reagent :refer [atom]]))
+
+
+;; Debugging in the Browser / JavaScript console
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (enable-console-print!)
 
 (println "This text is printed from src/clojure-virtual-study-group/core.cljs. Go ahead and edit it and see reloading in action.")
 
-;; define your app data so that it doesn't get over-written on reload
 
+;; Application State
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; State changes will come from API calls to YouTube (assuming that is possible)
+;; Otherwise state will be updated from content files (describing content and providing links)
+;; State will be used to control what data should be show, and presentation managed by CSS
+
+;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom {:text "Hello world!"}))
 
+
+
+;; Reagent components
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Ensure all components and sub-components are treated as reagent components,
+;; by placing them in [] rather than as function calls (e.g. in the reagent/render-component)
+;; and any component that render-component manages.
 
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
    [:h3 "Edit this and watch it change!"]])
+
+
+
+;; Reagent specific code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
