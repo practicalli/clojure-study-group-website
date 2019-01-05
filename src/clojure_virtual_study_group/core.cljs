9f852a621b55be9d61c3b512c4803f7fce286b4e
John Stevenson <john@jr0cket.co.uk>
2019-01-05 10:50:51 +0000

    Refactor broadcast card to take specific broadcast
    Updated the data model of broadcasts with more details to be included in the
    display.

--- src/clojure_virtual_study_group/core.cljs
+++ src/clojure_virtual_study_group/core.cljs
@@ -36,10 +36,56 @@
      :description "Learn to think Functionally with Clojure in a series of YouTube broadcasts"
      :copyright   "2019 Practicalli"}
     :broadcasts
-    {:CSG-001 {:url "https://www.youtube.com/watch?v=MZcuL4lRw5E"
-         :title "Overview of Clojure"
-         :description "Discussing the uses of Clojure and a tour of its syntax"}}}))
-
+    {:CSG-001
+     {:url "https://www.youtube.com/watch?v=MZcuL4lRw5E"
+      :title "Overview of Clojure"
+      :description "Discussing the uses of Clojure and a tour of its syntax"
+      :thumbnail ""}
+     :CSG-002
+     {:url "https://www.youtube.com/watch?v=y5TAHwZc1JE"
+      :title "REPL Driven Development - ClojureBridge Exercises"
+      :description "Fast feedback with the REPL and working through ClojureBridge London exercises."
+      :thumbnail ""}
+     :CSG-003
+     {:url "https://www.youtube.com/watch?v=PmSPKvlJk74"
+      :title "Structural editing"
+      :description "Editing Clojure without breaking the structure of the code"
+      :thumbnail ""}
+     :CSG-004
+     {:url "https://www.youtube.com/watch?v=ikW6Qk73K1s"
+      :title "Working with Data"
+      :description "Using hash-map to model and work with data"
+      :thumbnail ""}
+     :CSG-005
+     {:url "https://www.youtube.com/watch?v=LvissLmUNho"
+      :title "clojure.test"
+      :description "Writing unit tests in Clojure"
+      :thumbnail ""}
+     :CSG-006
+     {:url "https://www.youtube.com/watch?v=opM7fU7IAV8"
+      :title "Advent of Clojure"
+      :description "Various solutions to day 1 challenge"
+      :thumbnail ""}
+     :CSG-007
+     {:url "https://www.youtube.com/watch?v=WYaIy3E6nLk"
+      :title "Reagent - a simple website"
+      :description "Intro to Clojurescript and reagent"
+      :thumbnail ""}
+     :CSG-008
+     {:url "https://www.youtube.com/watch?v=8u8y73zh0w0"
+      :title "4Clojure continued"
+      :description "Working through exercises 16 to 22"
+      :thumbnail ""}
+     :CSG-009
+     {:url "https://www.youtube.com/watch?v=_eO7EsXO2XE"
+      :title "Functional TicTacToe"
+      :description "Functional approach to building the TicTacToe game"
+      :thumbnail ""}
+     :CSG-010
+     {:url ""
+      :title ""
+      :description ""
+      :thumbnail ""}}}))
 
 
 ;; Reagent components
@@ -73,7 +119,7 @@
          :class "btn btn-primary"}
      "Play Broadcast"]]])
 
-(defn broadcast-card []
+#_(defn broadcast-card []
   [:div {:class "card mb-4 shadow-sm"
          :width "18rem"}
    [:img {:src "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Clojure_logo.svg/200px-Clojure_logo.svg.png"}]
@@ -86,6 +132,23 @@
          :class "btn btn-primary"}
      "Play Broadcast"]]])
 
+;; Refactor broadcast-card to take a specific map of details for a broadcast.
+;; This makes the broadcast-card less coupled to the application state
+;;
+
+(defn broadcast-card [broadcast-details]
+  [:div {:class "card mb-4 shadow-sm"
+         :width "36rem"}
+   [:img {:src (get broadcast-card :thumbnail)}]
+   [:div {:class "card-body"}
+    [:h5 {:class "card-title"}
+     (get broadcast-details :title)]
+    [:p {:class "card-text"}
+     (get broadcast-details  :description)]
+    [:a {:href (get broadcast-details :url)
+         :class "btn btn-primary"}
+     "Play Broadcast"]]])
+
 
 
 (defn website-title []
@@ -93,7 +156,7 @@
    [:h1 (get-in @app-state [:website :title])]
    [:h4 (get-in @app-state [:website :description])]])
 
-(defn study-group-website []
+#_(defn study-group-website []
   [:div {:class "container"}
    [website-title]
 
@@ -117,6 +180,24 @@
     [:div {:class "col-md-3"}
      [broadcast-card]]
     ]
+   ])
+
+
+(defn broadcast-carousel
+  "Displays the broadcasts as a carousel using Bootstrap"
+  [broadcasts]
+  [:div {:class "row"}
+   (for [broadcast broadcasts]
+     [:div {:class "col-md-3"}
+      [broadcast-card broadcast]])])
+
+
+(defn study-group-website []
+  [:div {:class "container"}
+   [website-title]
+
+   ;; Broadcasts
+   [broadcast-carousel (@app-state :broadcasts)]
    ])
 
 
@@ -166,3 +247,7 @@
   {:CSG-001 {:url "https://www.youtube.com/watch?v=MZcuL4lRw5E"
              :title "Overview of Clojure"
              :description "Discussing the uses of Clojure and a tour of its syntax"}}})
+
+
+
+(@app-state :broadcasts)
